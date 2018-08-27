2ec3f7b436a383d6b5d79f905faba54cb46ec280
senandhinil <tamilallinall0@gmail.com>
2018-08-27 15:50:38 +0530

    footer bup

---
+++ includes/footer.php
@@ -0,0 +1,73 @@
+	     </div>
+     </div>
+   </div>
+   <footer class="footer">
+	   <div class="container-fluid">
+		  <div class="copyright float-right">
+			 <?=date('Y')?> © CSS Corp | Powered by: ETAS Data Engineering Team.
+		  </div>
+	   </div>
+	</footer>
+		<div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 0px;">
+		   <div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div>
+		</div>
+		<div class="ps-scrollbar-y-rail" style="top: 0px; right: 0px; height: 582px;">
+		   <div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 445px;"></div>
+		</div>
+	 </div>
+	</div>
+      <!--   Core JS Files   -->
+      <script src="./assets/jquery.min.js" type="text/javascript"></script>
+      <script src="./assets/popper.min.js" type="text/javascript"></script>
+      <script src="./assets/bootstrap-material-design.min.js" type="text/javascript"></script>
+      <script src="./assets/perfect-scrollbar.jquery.min.js"></script>
+      <!-- Plugin for the momentJs  -->
+      <script src="./assets/moment.min.js"></script>
+      <!--  Plugin for Sweet Alert -->
+      <script src="./assets/sweetalert2.js"></script>
+      <!-- Forms Validations Plugin -->
+      <script src="./assets/jquery.validate.min.js"></script>
+      <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+      <script src="./assets/bootstrap-selectpicker.js"></script>
+      <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
+      <script src="./assets/bootstrap-datetimepicker.min.js"></script>
+      <!--  DataTables.net Plugin, full documentation here: https://datatables.net/    -->
+      <script src="./assets/jquery.dataTables.min.js"></script>
+      <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
+      <script src="./assets/core.js"></script>
+      <!-- Place this tag in your head or just before your close body tag. -->
+      <script async="" defer="" src="./assets/buttons.js"></script>
+      <!--  Notifications Plugin    -->
+      <script src="./assets/bootstrap-notify.js"></script>
+	  
+      <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+	  <script src="./assets/material-dashboard.min.js" type="text/javascript"></script>
+      <!-- Material Dashboard DEMO methods, don't include it in your project! -->
+      <script src="./assets/demo.js"></script>
+	  
+	  <script>
+	  
+      </script>
+      <div class="tooltip fade bs-tooltip-bottom" role="tooltip" id="tooltip16533" x-placement="bottom" style="position: absolute; top: 5px; left: -13px; will-change: top, left;" x-out-of-boundaries="">
+         <div class="arrow" style="left: 7px;"></div>
+         <div class="tooltip-inner">Remove</div>
+      </div>
+      <div class="tooltip fade bs-tooltip-bottom" role="tooltip" id="tooltip987211" x-placement="bottom" style="position: absolute; top: 5px; left: -13px; will-change: top, left;" x-out-of-boundaries="">
+         <div class="arrow" style="left: 7px;"></div>
+         <div class="tooltip-inner">Edit</div>
+      </div>
+      <div class="tooltip fade bs-tooltip-bottom" role="tooltip" id="tooltip765276" x-placement="bottom" style="position: absolute; top: 294px; left: 1085px; will-change: top, left;">
+         <div class="arrow" style="left: 59px;"></div>
+         <div class="tooltip-inner">Refresh</div>
+      </div>
+   </body>
+</html>
+<style>
+.copyright {
+    font-size: 14px;
+}
+.navbar {
+       background-color: #2a2929!important;
+       color: #fff;
+   }
+</style>
