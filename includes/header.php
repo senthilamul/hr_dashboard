87a06f1873fb8029cdf2d8f8d151cd0135c72040
senandhinil <tamilallinall0@gmail.com>
2018-08-27 15:50:23 +0530

    header bup

---
+++ includes/header.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en" class="perfect-scrollbar-on">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <link rel="icon" type="image/png" href="assets/img/favicon.png">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+      <title>
+         HR Attrition
+      </title>
+      <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no" name="viewport">
+      <link rel="stylesheet" type="text/css" href="./assets/css.css">
+      <link rel="stylesheet" href="./assets/font-awesome.min.css">
+      <link href="./assets/material-dashboard.min.css" rel="stylesheet">
+      <link href="./assets/demo.css" rel="stylesheet">
+    </head> 
+   <body class="">
+      <div class="wrapper ">
+         <div class="sidebar" data-color="rose" data-background-color="black" data-image="./assets/img/sidebar-1.jpg">
+            <div class="logo"><a href="#" class="simple-text logo-mini">
+               CT 
+               </a>
+               <a href="#" class="simple-text logo-normal">
+               Creative Tim 
+               </a>
+            </div>
+            <div class="sidebar-wrapper ps-container ps-theme-default ps-active-y" data-ps-id="abf28d0a-fbf7-29d5-f014-6ceff038e829">
+               <div class="user">
+                  <div class="photo">
+                     <img src="./assets/avatar.jpg">
+                  </div>
+                  <div class="user-info">
+                     <a data-toggle="collapse" href="#collapseExample" class="username collapsed" aria-expanded="false">
+                     <span>
+                     Tania Andrew 
+                     <b class="caret"></b>
+                     </span>
+                     </a>
+                     <div class="collapse" id="collapseExample" style="">
+                        <ul class="nav">
+                           <li class="nav-item">
+                              <a class="nav-link" href="#">
+                              <span class="sidebar-mini"> MP </span>
+                              <span class="sidebar-normal"> My Profile </span>
+                              </a>
+                           </li>
+                           <li class="nav-item">
+                              <a class="nav-link" href="#">
+                              <span class="sidebar-mini"> EP </span>
+                              <span class="sidebar-normal"> Edit Profile </span>
+                              </a>
+                           </li>
+                           <li class="nav-item">
+                              <a class="nav-link" href="#">
+                              <span class="sidebar-mini"> S </span>
+                              <span class="sidebar-normal"> Settings </span>
+                              </a>
+                           </li>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+               <ul class="nav">
+                  <li class="nav-item active ">
+                     <a class="nav-link" href="dashboard.html">
+                        <i class="material-icons">dashboard</i>
+                        <p> Dashboard </p>
+                     </a>
+                  </li>
+                  <li class="nav-item ">
+                     <a class="nav-link" data-toggle="collapse" href="#pagesExamples">
+                        <i class="material-icons">list</i>
+                        <p> Upload CSV 
+                           <b class="caret"></b>
+                        </p>
+                     </a>
+                     <div class="collapse" id="pagesExamples">
+                        <ul class="nav">
+                           <li class="nav-item ">
+                              <a class="nav-link" href="uploadForm.php">
+                              <span class="sidebar-mini"> F </span>
+                              <span class="sidebar-normal"> Form </span>
+                              </a>
+                           </li>
+                        </ul>
+                     </div>
+                  </li>
+               </ul>
+               <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 0px;">
+                  <div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div>
+               </div>
+               <div class="ps-scrollbar-y-rail" style="top: 0px; height: 556px; right: 0px;">
+                  <div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 504px;"></div>
+               </div>
+            </div>
+            <div class="sidebar-background" style="background-image: url(./assets/img/sidebar-1.jpg) "></div>
+         </div>
+         <div class="main-panel ps-container ps-theme-default ps-active-y" data-ps-id="bc8a03c5-5fb0-f304-0bf6-cd23590ebf6f">
+            <!-- Navbar -->
+            <nav class="navbar navbar-expand-lg navbar-absolute fixed-top " id="navigation-example">
+               <div class="container-fluid">
+                  <div class="navbar-wrapper">
+                     <div class="navbar-minimize">
+                        <button id="minimizeSidebar" class="btn btn-just-icon btn-white btn-fab btn-round">
+                        <i class="material-icons text_align-center visible-on-sidebar-regular">more_vert</i>
+                        <i class="material-icons design_bullet-list-67 visible-on-sidebar-mini">view_list</i>
+                        </button>
+                     </div>
+                     <a class="navbar-brand" href="#pablo">Dashboard</a>
+                  </div>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation" data-target="#navigation-example">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="navbar-toggler-icon icon-bar"></span>
+                  <span class="navbar-toggler-icon icon-bar"></span>
+                  <span class="navbar-toggler-icon icon-bar"></span>
+                  </button>
+                  <div class="collapse navbar-collapse justify-content-end">
+                     <ul class="navbar-nav">
+                        <li class="nav-item">
+                           <a class="nav-link" href="#pablo">
+                              <i class="material-icons">person</i>
+                              <p class="d-lg-none d-md-block">
+                                 Account
+                              </p>
+                              <div class="ripple-container"></div>
+                           </a>
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+            </nav>
+            <!-- End Navbar -->
+               <div class="content">
+                  <div class="content">
+                     <div class="container-fluid">
